41a5706949ac478f07254f3739119dedcca105da
Daem Pasha <daempasha@gmail.com>
2022-12-27 23:36:02 +0000

    Added dotenv and taking in port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(cors())
 
 app.get("/", (req, res) => { res.send("Hello") })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("app started")
 })
