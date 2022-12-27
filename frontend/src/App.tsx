a1d5c773f533481c593611ccec74d2a622127237
Daem Pasha <daempasha@gmail.com>
2022-12-27 22:54:25 +0000

    Minimal app for deployment testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios.get(`http://localhost:8000`).then(response => setData(response.data))
+  }, [])
 
   return (
     <div className="App">
       <p>Test</p>
+      <p>Api data: {data}</p>
     </div>
   )
 }
