57117f81b936380c6df441346f82d23d8e880722
Daem Pasha <daempasha@gmail.com>
2022-12-30 16:07:37 +0000

    Added focus for button

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 const Button: React.FC<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>> = ({ ...props }) => {
-    return <button className="p-1 block w-full text-white rounded-md bg-blue-400 hover:bg-blue-500 active:bg-blue-300 transition-colors" {...props} />
+    return <button className="p-1 block w-full text-white rounded-md bg-blue-400 hover:bg-blue-500 active:bg-blue-300 transition-colors outline-none focus:ring-2 ring-offset-2 ring-blue-400" {...props} />
 }
 
 export default Button;
